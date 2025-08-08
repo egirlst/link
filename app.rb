b6398575b3f954f3193b52e02d9c760a052bce1f
TESS <hi@egirl.st>
2025-08-08 18:25:41 +0100

    Create app.rb

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'json'
+
+PASSWORD = 'ghjk'
+DATA_FILE = 'shortcuts.json'
+
+def load_shortcuts
+  if File.exist?(DATA_FILE)
+    JSON.parse(File.read(DATA_FILE))
+  else
+    {}
+  end
+end
+
+def save_shortcuts(shortcuts)
+  File.write(DATA_FILE, JSON.pretty_generate(shortcuts))
+end
+
+SHORTCUTS = load_shortcuts
+
+post '/create' do
+  return status 401 unless params['password'] == PASSWORD
+  name = params['name']
+  link = params['link']
+  halt 400, "Missing name or link" unless name && link
+
+  SHORTCUTS[name] = link
+  save_shortcuts(SHORTCUTS)
+
+  "Shortcut '#{name}' created."
+end
+
+get '/:name' do
+  link = SHORTCUTS[params['name']]
+  return status 404 unless link
+  redirect link
+end
