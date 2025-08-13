28933402f2c5a9fd6f5d33bb78ebc2b2796e40fb
TESS <hi@egirl.st>
2025-08-13 17:37:54 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,29 +1,19 @@
 require 'sinatra'
 require 'json'
 
-# Configure Sinatra to bind to all interfaces and port 3099
-set :bind, '0.0.0.0'
-set :port, 3099
-
-# Add debugging middleware to see what host header we're getting
-use Rack::CommonLogger
-before do
-  puts "Host header: #{request.host}"
-  puts "All headers: #{request.env.select { |k,v| k.start_with?('HTTP_') }}"
+# Disable Rack Host Authorization entirely
+if Rack::Request.method_defined?(:host)
+  use Rack::HostAuthorization, []  # Empty list allows all hosts
 end
 
-# Temporarily disable host authorization to debug
-set :protection, except: [:host_authorization]
+set :bind, '0.0.0.0'
+set :port, 3099
 
 PASSWORD = 'ghjk'
 DATA_FILE = 'shortcuts.json'
 
 def load_shortcuts
-  if File.exist?(DATA_FILE)
-    JSON.parse(File.read(DATA_FILE))
-  else
-    {}
-  end
+  File.exist?(DATA_FILE) ? JSON.parse(File.read(DATA_FILE)) : {}
 end
 
 def save_shortcuts(shortcuts)
@@ -32,15 +22,13 @@ end
 
 SHORTCUTS = load_shortcuts
 
-# Temporary test route
 get '/' do
   "URL Shortener is working! Host: #{request.host}"
 end
 
 post '/create' do
-  return status 401 unless params['password'] == PASSWORD
-  name = params['name']
-  link = params['link']
+  halt 401 unless params['password'] == PASSWORD
+  name, link = params.values_at('name', 'link')
   halt 400, "Missing name or link" unless name && link
   SHORTCUTS[name] = link
   save_shortcuts(SHORTCUTS)
@@ -49,8 +37,6 @@ end
 
 get '/:name' do
   link = SHORTCUTS[params['name']]
-  return status 404 unless link
+  halt 404 unless link
   redirect link
 end
-
-# Server will start automatically with Puma
